77a1e61f4b2a5b8838431c601df77ac7025a65b8
Emil Čelustka <emil.celustka@gmail.com>
2020-10-18 16:30:05 +0200

    refactor: scss function add try catch

--- plugins/scss.js
+++ plugins/scss.js
@@ -4,18 +4,22 @@ const sass = require('node-sass-promise')
 const fsAsync = fs.promises
 
 module.exports = async (scssPath, cssPath) => {
-    const cssDir = fs.existsSync(path.dirname(cssPath))
+    try {
+        const cssDir = fs.existsSync(path.dirname(cssPath))
 
-    //If cssPath directory doesn't exist...
-    if (!cssDir) {
-        await fsAsync.mkdir(path.dirname(cssPath), {
-            recursive: true,
-        })
+        //If cssPath directory doesn't exist...
+        if (!cssDir) {
+            await fsAsync.mkdir(path.dirname(cssPath), {
+                recursive: true,
+            })
+        }
+
+        //Render css from sass
+        const file = await sass.render({ file: scssPath })
+
+        // Write result css string to cssPath file
+        await fsAsync.writeFile(cssPath, file.css.toString())
+    } catch (error) {
+        throw new Error(error)
     }
-
-    //Render css from sass
-    const file = await sass.render({ file: scssPath })
-
-    // Write result css string to cssPath file
-    await fsAsync.writeFile(cssPath, file.css.toString())
 }
