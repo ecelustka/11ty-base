d92aa9ec9cef14c6d2ae47671bab1aeabade51ed
Emil Čelustka <emil.celustka@gmail.com>
2021-02-07 10:37:55 +0100

    add env variables processing

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,30 @@
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import { config } from 'dotenv'
 import { terser } from 'rollup-plugin-terser'
 
 const dev = process.env.ELEVENTY_ENV !== 'production'
 
-// prettier-ignore
+const envVariables = config().parsed
+
 export default {
     input: 'src/js/index.js',
     output: {
         sourcemap: false,
         format: 'iife',
-        file: '_site/js/index.bundle.js'
+        file: '_site/js/index.bundle.js',
     },
     plugins: [
         resolve({ browser: true }),
+        replace({
+            process: JSON.stringify({
+                env: {
+                    ...envVariables,
+                },
+            }),
+        }),
         commonjs(),
         babel(),
         !dev && terser(),
