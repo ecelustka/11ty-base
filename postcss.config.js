204071801deffeba83e281381aacd4a5ad1d34b6
Emil Čelustka <emil.celustka@gmail.com>
2021-02-07 10:41:41 +0100

    refactor: purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,10 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
     from: undefined,
     map: false,
     plugins: [
         require('autoprefixer'),
         ...(process.env.ELEVENTY_ENV === 'production'
-            ? [
-                  require('cssnano'),
-                  purgecss({
-                      content: ['./**/*.html'],
-                  }),
-            ]
+            ? [require('cssnano')]
             : []),
     ],
 }
