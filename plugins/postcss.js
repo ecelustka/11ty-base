b0f43a37bdc8d490ac3c21bcb034472295a7110b
Emil Čelustka <emil.celustka@gmail.com>
2021-02-09 15:02:14 +0100

    refactor: postcss plugin

--- plugins/postcss.js
+++ plugins/postcss.js
@@ -9,38 +9,35 @@ const defaultOptions = {
 
 module.exports = (eleventyInstance, options = defaultOptions) => {
     try {
-        if (options.srcFiles instanceof Array) {
-            options.srcFiles.forEach(async (item) => {
-                const postcssDir = fs.existsSync(path.dirname(item))
-
-                //If postCssPath directory doesn't exist...
-                if (!postcssDir) {
-                    throw new Error('POSTCSS plugin: CSS file does not exists.')
-                }
-
-                // Read file
-                const file = fs.readFileSync(item, 'utf-8')
-
-                // Load postcss plugins and options
-                const { plugins, options } = await postcssConfig()
-
-                // process file
-                const processedFile = await postcss(plugins).process(
-                    file,
-                    options
-                )
-
-                // write processed file
-                fs.writeFileSync(item, processedFile.css)
-
-                // if maps, write processed maps
-                if (processedFile.map) {
-                    fs.writeFileSync(`${item}.map`, processedFile.map)
-                }
-            })
-        } else {
+        if (!Array.isArray(options.srcFiles)) {
             throw new Error("POSTCSS plugin: key srcFiles isn't an Array")
         }
+
+        options.srcFiles.forEach(async (item) => {
+            const postcssDir = fs.existsSync(path.dirname(item))
+
+            //If postCssPath directory doesn't exist...
+            if (!postcssDir) {
+                throw 'POSTCSS plugin: CSS file does not exists.'
+            }
+
+            // Read file
+            const file = fs.readFileSync(item, 'utf-8')
+
+            // Load postcss plugins and options
+            const { plugins, options } = await postcssConfig()
+
+            // process file
+            const processedFile = await postcss(plugins).process(file, options)
+
+            // write processed file
+            fs.writeFileSync(item, processedFile.css)
+
+            // if maps, write processed maps
+            if (processedFile.map) {
+                fs.writeFileSync(`${item}.map`, processedFile.map)
+            }
+        })
     } catch (error) {
         throw new Error(error)
     }
